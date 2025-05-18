2f8bd1be98b4408f7e7aec176cd5dd4eaa50dee9
Backend-beomcheol <kim.beomcheol@cashwalk.io>
2025-05-19 01:06:08 +0900

    코드 정리

--- packages/gateway-server/src/common/config/validation-schema.ts
+++ packages/gateway-server/src/common/config/validation-schema.ts
@@ -3,7 +3,5 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   PORT: Joi.number().default(3000),
 
-  JWT_SECRET: Joi.string().required(),
-
   MONGODB_HOST: Joi.string().required(),
 });
